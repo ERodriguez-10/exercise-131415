81a1fb021823d4f7f71232bbcc07fbc195b6b882
ERODRIGUEZ-10 <emiliano.rodriguez@edit.com.ar>
2022-09-30 12:07:28 -0300

    Solution exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+import useList from './hooks/useList';
+import LEVELS from './models/levels.enum';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const TaskSchema = Yup.object().shape({
+    name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    description: Yup.string().min(2, 'Too Short!').max(100, 'Too Long!').required('Required'),
+  });
+  const defaultTask = {
+    name: 'Formik',
+    description: 'Formulario con Formik',
+    level: LEVELS.URGENT,
+    done: false,
+  };
+  const tasks = useList([defaultTask]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='App'>Task List</h1>
+      <Formik
+        initialValues={{
+          name: '',
+          description: '',
+          level: LEVELS.NORMAL,
+          done: false,
+        }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            tasks.push(values);
+            actions.resetForm({});
+            actions.setSubmitting(false);
+          }, 500);
+        }}
+        validationSchema={TaskSchema}
+      >
+        {({ errors }) => (
+          <Form className='App-header'>
+            <h6>Create a New Task</h6>
+            <div>
+              <Field name="name" placeholder="Task Name" className="name-text" />
+              <p className='errors'>{errors && errors.name}</p>
+            </div>
+            <div className='errors-text'>
+              <Field name="description" as="textarea" placeholder="Task Description" className="description-text" />
+              <p className='errors'>{errors && errors.description}</p>
+            </div>
+            <Field as="select" name="level" className="select-option">
+              <option value={LEVELS.NORMAL}>Normal</option>
+              <option value={LEVELS.URGENT}>Urgent</option>
+              <option value={LEVELS.BLOCKING}>Blocking</option>
+            </Field>
+            <button type="submit" className='submit-btn'>Submit</button>
+          </Form>
+        )}
+      </Formik>
+      {tasks.isEmpty() ? (
+        <p className='App'>Task List is Empty</p>
+      ) : (
+        <div className='App-List'>
+          {tasks.value.map((task, index) => (
+            <li key={index} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <input type="checkbox" onClick={() => tasks.remove(index)} checked={false} />
+              <p style={{ fontWeight: 'bold', marginRight: '5px' }}>{task.name}</p>
+              <p>{task.description}</p>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
